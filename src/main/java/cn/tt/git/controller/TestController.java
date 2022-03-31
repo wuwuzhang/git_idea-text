04392d1f78b24fe994b574fec0e75924ead6e798
wuwu <tt_wuwu@123.com>
2022-03-31 16:40:10 +0800

    这是Dev分支第一次提交

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -3,5 +3,6 @@ package cn.tt.git.controller;
 public class TestController {
     public static void main(String[] args){
         System.out.println("hello git!!!");
+        System.out.println("hello git dev");
     }
 }
